f08d2d615463e549a1f937a58094b777fe8cf4f7
dharmendra kumar <dharmendra.kumar@hmcts.net>
2017-12-13 23:21:16 +0000

    added health end point

--- src/main/java/uk/gov/hmcts/reform/authorisation/ServiceAuthorisationApi.java
+++ src/main/java/uk/gov/hmcts/reform/authorisation/ServiceAuthorisationApi.java
@@ -5,9 +5,14 @@ import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import uk.gov.hmcts.reform.authorisation.healthcheck.InternalHealth;
 
 import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
+import static org.springframework.http.MediaType.APPLICATION_JSON_UTF8_VALUE;
 
 @EnableFeignClients
 @FeignClient(name = "idam-s2s-auth", url = "${idam.s2s-auth.url}")
@@ -19,4 +24,11 @@ public interface ServiceAuthorisationApi {
     @GetMapping(value = "/authorisation-check")
     void authorise(@RequestHeader(AUTHORIZATION) final String authHeader,
                    @RequestParam("role") final String[] roles);
+
+    @RequestMapping(
+            method = RequestMethod.GET,
+            value = "/health",
+            headers = CONTENT_TYPE + "=" + APPLICATION_JSON_UTF8_VALUE
+    )
+    InternalHealth health();
 }
---
+++ src/main/java/uk/gov/hmcts/reform/authorisation/healthcheck/ServiceAuthHealthIndicator.java
@@ -0,0 +1,33 @@
+package uk.gov.hmcts.reform.authorisation.healthcheck;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.stereotype.Component;
+import uk.gov.hmcts.reform.authorisation.ServiceAuthorisationApi;
+
+@Component
+public class ServiceAuthHealthIndicator implements HealthIndicator {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ServiceAuthHealthIndicator.class);
+
+    private final ServiceAuthorisationApi serviceAuthorisationApi;
+
+    @Autowired
+    public ServiceAuthHealthIndicator(final ServiceAuthorisationApi serviceAuthorisationApi) {
+        this.serviceAuthorisationApi = serviceAuthorisationApi;
+    }
+
+    @Override
+    public Health health() {
+        try {
+            InternalHealth internalHealth = this.serviceAuthorisationApi.health();
+            return new Health.Builder(internalHealth.getStatus(), internalHealth.getDetails()).build();
+        } catch (Exception ex) {
+            LOGGER.error("Error on service auth healthcheck", ex);
+            return Health.down(ex).build();
+        }
+    }
+}
