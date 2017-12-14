257d610ce2f80d14b9c64818fe272d0c4e8a3876
dharmendra kumar <dharmendra.kumar@hmcts.net>
2017-12-14 08:00:49 +0000

    resolved status

--- src/main/java/uk/gov/hmcts/reform/authorisation/healthcheck/ServiceAuthHealthIndicator.java
+++ src/main/java/uk/gov/hmcts/reform/authorisation/healthcheck/ServiceAuthHealthIndicator.java
@@ -24,7 +24,7 @@ public class ServiceAuthHealthIndicator implements HealthIndicator {
     public Health health() {
         try {
             InternalHealth internalHealth = this.serviceAuthorisationApi.health();
-            return new Health.Builder(internalHealth.getStatus(), internalHealth.getDetails()).build();
+            return new Health.Builder(internalHealth.getStatus()).build();
         } catch (Exception ex) {
             LOGGER.error("Error on service auth healthcheck", ex);
             return Health.down(ex).build();
