045f90187378eccffe13167f60c06fd75bd881b6
dharmendra kumar <dharmendra.kumar@hmcts.net>
2017-12-15 19:38:46 +0000

    Make s2s api without feign decoder (#13)

--- src/main/java/uk/gov/hmcts/reform/authorisation/ServiceAuthorisationApi.java
+++ src/main/java/uk/gov/hmcts/reform/authorisation/ServiceAuthorisationApi.java
@@ -1,25 +1,14 @@
 package uk.gov.hmcts.reform.authorisation;
 
-import feign.codec.Decoder;
-import feign.jackson.JacksonDecoder;
 import org.springframework.cloud.netflix.feign.FeignClient;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Primary;
-import org.springframework.context.annotation.Scope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import uk.gov.hmcts.reform.authorisation.healthcheck.InternalHealth;
 
 import static org.springframework.http.HttpHeaders.AUTHORIZATION;
-import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
-import static org.springframework.http.MediaType.APPLICATION_JSON_UTF8_VALUE;
 
-@FeignClient(name = "idam-s2s-auth", url = "${idam.s2s-auth.url}",
-        configuration = ServiceAuthorisationApi.ServiceAuthConfiguration.class)
+@FeignClient(name = "idam-s2s-auth", url = "${idam.s2s-auth.url}")
 public interface ServiceAuthorisationApi {
     @PostMapping(value = "/lease")
     String serviceToken(@RequestParam("microservice") final String microservice,
@@ -28,21 +17,4 @@ public interface ServiceAuthorisationApi {
     @GetMapping(value = "/authorisation-check")
     void authorise(@RequestHeader(AUTHORIZATION) final String authHeader,
                    @RequestParam("role") final String[] roles);
-
-    @RequestMapping(
-            method = RequestMethod.GET,
-            value = "/health",
-            headers = CONTENT_TYPE + "=" + APPLICATION_JSON_UTF8_VALUE
-    )
-    InternalHealth health();
-
-
-    class ServiceAuthConfiguration {
-        @Bean
-        @Primary
-        @Scope("prototype")
-        Decoder feignDecoder() {
-            return new JacksonDecoder();
-        }
-    }
 }
---
+++ src/main/java/uk/gov/hmcts/reform/authorisation/ServiceAuthorisationHealthApi.java
@@ -0,0 +1,35 @@
+package uk.gov.hmcts.reform.authorisation;
+
+import feign.codec.Decoder;
+import feign.jackson.JacksonDecoder;
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Scope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import uk.gov.hmcts.reform.authorisation.healthcheck.InternalHealth;
+
+import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
+import static org.springframework.http.MediaType.APPLICATION_JSON_UTF8_VALUE;
+
+@FeignClient(name = "idam-s2s-auth-health", url = "${idam.s2s-auth.url}",
+        configuration = ServiceAuthorisationHealthApi.ServiceAuthConfiguration.class)
+public interface ServiceAuthorisationHealthApi {
+
+    @RequestMapping(
+            method = RequestMethod.GET,
+            value = "/health",
+            headers = CONTENT_TYPE + "=" + APPLICATION_JSON_UTF8_VALUE
+    )
+    InternalHealth health();
+
+    class ServiceAuthConfiguration {
+        @Bean
+        @Primary
+        @Scope("prototype")
+        Decoder feignDecoder() {
+            return new JacksonDecoder();
+        }
+    }
+}
