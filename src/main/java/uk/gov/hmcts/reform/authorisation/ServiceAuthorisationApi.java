a0c2e695773652b121982d7e79f0b864c0f69ddd
dharmendra kumar <dharmendra.kumar@hmcts.net>
2017-12-15 19:30:44 +0000

    resolved import

--- src/main/java/uk/gov/hmcts/reform/authorisation/ServiceAuthorisationApi.java
+++ src/main/java/uk/gov/hmcts/reform/authorisation/ServiceAuthorisationApi.java
@@ -1,22 +1,12 @@
 package uk.gov.hmcts.reform.authorisation;
 
-import feign.codec.Decoder;
-import feign.jackson.JacksonDecoder;
 import org.springframework.cloud.netflix.feign.FeignClient;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Primary;
-import org.springframework.context.annotation.Scope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import uk.gov.hmcts.reform.authorisation.healthcheck.InternalHealth;
 
 import static org.springframework.http.HttpHeaders.AUTHORIZATION;
-import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
-import static org.springframework.http.MediaType.APPLICATION_JSON_UTF8_VALUE;
 
 @FeignClient(name = "idam-s2s-auth", url = "${idam.s2s-auth.url}")
 public interface ServiceAuthorisationApi {
--- src/main/java/uk/gov/hmcts/reform/authorisation/healthcheck/ServiceAuthHealthIndicator.java
+++ src/main/java/uk/gov/hmcts/reform/authorisation/healthcheck/ServiceAuthHealthIndicator.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.stereotype.Component;
-import uk.gov.hmcts.reform.authorisation.ServiceAuthorisationApi;
 import uk.gov.hmcts.reform.authorisation.ServiceAuthorisationHealthApi;
 
 @Component
