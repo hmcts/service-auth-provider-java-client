95f780c330fb798b1bd39215f75b33b4e794d647
Rafał Lewandowski <r.lewandowski@kainos.com>
2017-12-20 17:07:11 +0000

    Address review comments

--- src/main/java/uk/gov/hmcts/reform/authorisation/generators/AuthTokenGeneratorFactory.java
+++ src/main/java/uk/gov/hmcts/reform/authorisation/generators/AuthTokenGeneratorFactory.java
@@ -2,9 +2,11 @@ package uk.gov.hmcts.reform.authorisation.generators;
 
 import uk.gov.hmcts.reform.authorisation.ServiceAuthorisationApi;
 
+import java.time.Duration;
+
 public class AuthTokenGeneratorFactory {
 
-    public AuthTokenGenerator createDefaultGenerator(
+    public static AuthTokenGenerator createDefaultGenerator(
             String secret,
             String microService,
             ServiceAuthorisationApi serviceAuthorisationApi
@@ -14,6 +16,24 @@ public class AuthTokenGeneratorFactory {
                         new ServiceAuthTokenGenerator(secret, microService, serviceAuthorisationApi)
                 )
         );
+    }
+
+    public static AuthTokenGenerator createDefaultGenerator(
+            String secret,
+            String microService,
+            ServiceAuthorisationApi serviceAuthorisationApi,
+            Duration refreshTimeDelta
+    ) {
+        return new BearerTokenGenerator(
+                new AutorefreshingJwtAuthTokenGenerator(
+                        new ServiceAuthTokenGenerator(secret, microService, serviceAuthorisationApi),
+                        refreshTimeDelta
+                )
+        );
+    }
+
+    private AuthTokenGeneratorFactory() {
+        // Static factory class
     }
 
 }
