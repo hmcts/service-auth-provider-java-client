a0c7f2892c6221c6a4d4baea64d2b3a783036c7b
Nitin Prabhu <nitinprabhu@users.noreply.github.com>
2018-03-20 18:38:18 +0000

    Update client to use spring annotations (#29)
    * Updated Client to send json in request body
    
    * update key in the request body
    
    * Updated service token api to use spring annotation instead of feign

--- src/main/java/uk/gov/hmcts/reform/authorisation/ServiceAuthorisationApi.java
+++ src/main/java/uk/gov/hmcts/reform/authorisation/ServiceAuthorisationApi.java
@@ -1,22 +1,21 @@
 package uk.gov.hmcts.reform.authorisation;
 
-import feign.Body;
-import feign.Headers;
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Map;
 
 import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 
 @FeignClient(name = "idam-s2s-auth", url = "${idam.s2s-auth.url}")
 public interface ServiceAuthorisationApi {
     @PostMapping(value = "/lease")
-    @Headers("Content-Type: application/json")
-    @Body("\"%7B\"microservice\":\"{microservice}\",\"one_time_password\":\"{oneTimePassword}\"%7D")
-    String serviceToken(@RequestParam("microservice") final String microservice,
-                        @RequestParam("oneTimePassword") final String oneTimePassword);
+    String serviceToken(@RequestBody Map<String, String> signIn,
+                        @RequestHeader("Content-Type") String contentType);
 
     @SuppressWarnings("PMD.UseVarargs")
     @GetMapping(value = "/authorisation-check")
