ddcc01bdf0e98430da271d18167fa260fd11ef49
Kevlar <kevin.j.cross@gmail.com>
2018-09-24 15:35:22 +0100

    Set the decoder to a string decoder so that feign does not get confused with possible Json decoders via spring beans.

--- src/main/java/uk/gov/hmcts/reform/authorisation/ServiceAuthorisationApi.java
+++ src/main/java/uk/gov/hmcts/reform/authorisation/ServiceAuthorisationApi.java
@@ -1,7 +1,9 @@
 package uk.gov.hmcts.reform.authorisation;
 
+import feign.codec.Decoder;
+import feign.codec.StringDecoder;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.util.MimeTypeUtils;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,11 +13,19 @@ import org.springframework.web.bind.annotation.RequestParam;
 import java.util.Map;
 
 import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+import static org.springframework.util.MimeTypeUtils.APPLICATION_JSON_VALUE;
+import static org.springframework.util.MimeTypeUtils.TEXT_PLAIN_VALUE;
 
-@FeignClient(name = "idam-s2s-auth", url = "${idam.s2s-auth.url}")
+@FeignClient(
+        name = "idam-s2s-auth",
+        url = "${idam.s2s-auth.url}",
+        configuration = ServiceAuthorisationApi.Config.class
+)
 public interface ServiceAuthorisationApi {
 
-    @PostMapping(value = "/lease", consumes = MimeTypeUtils.APPLICATION_JSON_VALUE)
+    @PostMapping(value = "/lease",
+            consumes = APPLICATION_JSON_VALUE,
+            produces = TEXT_PLAIN_VALUE)
     String serviceToken(@RequestBody Map<String, String> signIn);
 
     @SuppressWarnings("PMD.UseVarargs")
@@ -25,4 +35,11 @@ public interface ServiceAuthorisationApi {
 
     @GetMapping(value = "/details")
     String getServiceName(@RequestHeader(AUTHORIZATION) final String authHeader);
+
+    class Config {
+        @Bean
+        Decoder stringDecoder() {
+            return new StringDecoder();
+        }
+    }
 }
