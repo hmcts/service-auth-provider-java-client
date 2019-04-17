1125165e3ad9ca0f6ce1cae412910bcd2cf8203b
alexis.gayte <alexis.gayte@gmail.com>
2019-04-17 16:25:49 +0100

    add condition on FeignAutoConfiguration

--- src/main/java/uk/gov/hmcts/reform/authorisation/ServiceAuthAutoConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/authorisation/ServiceAuthAutoConfiguration.java
@@ -1,13 +1,16 @@
 package uk.gov.hmcts.reform.authorisation;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import uk.gov.hmcts.reform.authorisation.healthcheck.ServiceAuthHealthIndicator;
 
 @Configuration
 @ConditionalOnProperty(prefix = "idam.s2s-auth", name = "url")
+@ConditionalOnClass(FeignAutoConfiguration.class)
 @EnableFeignClients(basePackageClasses = ServiceAuthorisationApi.class)
 public class ServiceAuthAutoConfiguration {
 
