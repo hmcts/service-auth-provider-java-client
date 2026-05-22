f7cb89bba5df5505b646c9a5499a2a0b84a0bbfa
Michael1142 <michael1142@hotmail.co.uk>
2026-05-22 14:23:34 +0100

    Spring 4 upgrade

--- src/main/java/uk/gov/hmcts/reform/authorisation/healthcheck/ServiceAuthHealthIndicator.java
+++ src/main/java/uk/gov/hmcts/reform/authorisation/healthcheck/ServiceAuthHealthIndicator.java
@@ -3,8 +3,8 @@ package uk.gov.hmcts.reform.authorisation.healthcheck;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.actuate.health.Health;
-import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.boot.health.contributor.Health;
+import org.springframework.boot.health.contributor.HealthIndicator;
 import uk.gov.hmcts.reform.authorisation.ServiceAuthorisationHealthApi;
 
 public class ServiceAuthHealthIndicator implements HealthIndicator {
