e18246319d4476c4f2a5f0236e4f0d9a5edaa64c
Michael1142 <michael1142@hotmail.co.uk>
2026-05-27 14:41:55 +0100

    Update imports to use spring boot 4 namespaces

--- src/main/java/uk/gov/hmcts/reform/authorisation/healthcheck/InternalHealth.java
+++ src/main/java/uk/gov/hmcts/reform/authorisation/healthcheck/InternalHealth.java
@@ -3,7 +3,7 @@ package uk.gov.hmcts.reform.authorisation.healthcheck;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import org.springframework.boot.actuate.health.Status;
+import org.springframework.boot.health.contributor.Status;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class InternalHealth {
